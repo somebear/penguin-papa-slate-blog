544746eaac4f373267ab6f7625dee559af37b713
banobunu <guibin1989@gmail.com>
2025-03-04 17:06:38 +0800

    feat: blog init

--- slate.config.ts
+++ slate.config.ts
@@ -5,19 +5,19 @@ import { defineConfig } from './src/helpers/config-helper';
 
 export default defineConfig({
   lang: 'en-US',
-  site: 'https://slate-blog-demo.vercel.app',
+  site: 'https://penguin-papa-slate-blog.vercel.app',
   avatar: '/avatar.png',
-  title: 'Slate Blog',
+  title: ' 小企鹅爸爸的生活',
   description: 'Pure thoughts, simple stories.',
   lastModified: true,
   readTime: true,
   footer: {
-    copyright: '© 2025 Slate Design',
+    copyright: '© 2025  小企鹅爸爸',
   },
   socialLinks: [
     {
       icon: 'github',
-      link: 'https://github.com/SlateDesign/slate-blog'
+      link: 'https://github.com/somebear'
     },
 ]
 });
---
+++ src/content/post/new_blog.md
@@ -0,0 +1,9 @@
+---
+title: 新的博客，新的开始
+description: 又开始折腾博客了
+tags: [Life]
+pubDate: 2025-04-16
+draft: false
+---
+# 新博客
+我又回来啦
